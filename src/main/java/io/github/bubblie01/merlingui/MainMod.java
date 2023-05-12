cd2aa2e6f0749b7e92eb02e700261ba914c6c540
kb1000 <kaeptmblaubaer1000@gmail.com>
2023-05-12 03:53:33 +0200

    Update to 1.19.4, actual Quilt port

--- src/main/java/io/github/bubblie01/merlingui/MainMod.java
+++ src/main/java/io/github/bubblie01/merlingui/MainMod.java
@@ -1,9 +1,12 @@
 package io.github.bubblie01.merlingui;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
@@ -13,11 +16,12 @@ public class MainMod implements ModInitializer
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String MOD_ID = "merlin-gui";
-	public static final TestItem TEST_ITEM = new TestItem(new Item.Settings().group(ItemGroup.MATERIALS));
+	public static final TestItem TEST_ITEM = new TestItem(new Item.Settings());
 	@Override
 	public void onInitialize(ModContainer modContainer)
 	{
 		//System.loadLibrary("renderdoc");
-		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "test_item"),TEST_ITEM);
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "test_item"),TEST_ITEM);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> entries.addItem(TEST_ITEM));
 	}
 }
